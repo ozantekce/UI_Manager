a9a3fd10c7b800293959c133f46afa2c4e58a4cf
ozantekce <ozantekce@gmail.com>
2023-09-01 14:44:23 +0300

    feat: TEXT_FORMATS

---
+++ Assets/Scripts/TEXT_FORMATS.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TEXT_FORMATS
+{
+
+
+
+    private static Dictionary<string, TextFormat> NameToFormat = new Dictionary<string, TextFormat>()
+    {
+        {"Gold"     ,       new TextFormat("G:{0}") },
+        {"Gold2"    ,       new TextFormat("Gold({0})") },
+        {"MAX"      ,       new TextFormat("MAX") },
+        {"Time"     ,       new TextFormat("H:{0} M:{1} S:{2}") },
+
+    };
+
+    public static string ExecuteFormat(this string formatName,params string[] texts)
+    {
+        if (string.IsNullOrEmpty(formatName))
+        {
+            string result = string.Empty;
+            foreach (string text in texts)
+            {
+                result += text;
+            }
+            return result;
+        }
+
+        return NameToFormat[formatName].ExecuteFormat(texts);
+    }
+
+
+    public static void AddFormat(string formatName, string format)
+    {
+        NameToFormat.Add(formatName,new TextFormat(format));
+    }
+
+
+    private class TextFormat
+    {
+
+        private string _format;
+
+        public TextFormat(string format)
+        {
+            _format = format;
+        }
+
+        public virtual string ExecuteFormat(params string[] texts)
+        {
+            return string.Format(this._format, texts);
+        }
+
+    }
+
+
+}
